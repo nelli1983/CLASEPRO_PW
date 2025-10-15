59257349ead8a4ad037f6c5a9a520917df9d12e4
nelli1983 <nelli.khachaturova@gmail.com>
2025-10-16 01:30:57 +0200

    Ejercicio2

--- Ejercicio1/src/Main.java
+++ Ejercicio1/src/Main.java
@@ -7,6 +7,6 @@ public class Main {
         System.out.println(nombre);
         System.out.println(edad);
         System.out.println(ciudad);
-        
+
     }
 }
---
+++ Ejercicio2/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int puntuacion = 0;
+        System.out.println("Puntuación inicial:"+puntuacion);
+        puntuacion = 5;
+        System.out.println("Después de primera modificación:"+puntuacion);
+        puntuacion = 10;
+        System.out.println("Después de segunda modificación:"+puntuacion);
+        puntuacion = 15;
+        System.out.println("Puntuación final:"+puntuacion);
+    }
+}
