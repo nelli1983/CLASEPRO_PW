97cff34ad27b4ead6a9119811f2c11442802aac7
nelli1983 <nelli.khachaturova@gmail.com>
2025-10-16 01:51:50 +0200

    Ejercicio3

---
+++ Ejercicio3/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        String name = "Carlos";
+        int edad = 30;
+        boolean estudiante = true;
+        double altura = 1.75;
+        char inicial = 'C';
+        System.out.println("Nombre:"+name);
+        System.out.println("Edad:"+edad);
+        System.out.println("¿Carlos es estudiante?:"+estudiante);
+        System.out.println("Altura:"+altura);
+        System.out.println("Inicial:"+inicial);
+
+
+
+
+        
+    }
+}
