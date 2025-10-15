a5e638f4e57f89043860b65f55b32336d9e50e26
nelli1983 <nelli.khachaturova@gmail.com>
2025-10-16 01:21:47 +0200

    Ejercicio1

---
+++ Ejercicio1/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        String nombre = "Ana";
+        int edad = 25;
+        String ciudad = "Madrid";
+
+        System.out.println(nombre);
+        System.out.println(edad);
+        System.out.println(ciudad);
+        
+    }
+}
