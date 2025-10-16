9d0fec48b88b719ad9857951eb395cceed5577dc
nelli1983 <nelli.khachaturova@gmail.com>
2025-10-16 02:01:16 +0200

    Ejercicio4

---
+++ Ejercicio4/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        String titulo = "Don Quijote de la Mancha";
+        String autor = "Miguel de Cervantes";
+        int publicacion = 1605;
+        int paginas = 863;
+        boolean disponibilidad = true;
+        System.out.println("Título:" + titulo);
+        System.out.println("Autor:" + autor);
+        System.out.println("Año de la publicación:" + publicacion);
+        System.out.println("Número de páginas:" + paginas);
+        System.out.println("¿Disponible en la biblioteca?" + disponibilidad);
+
+    }
+}
