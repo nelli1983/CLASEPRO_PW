c62e9a84525527fd1f45390672a2598d1eea2912
nelli1983 <nelli.khachaturova@gmail.com>
2025-11-25 05:16:22 +0100

    ProgramacionTema1Ejercicio8

---
+++ ProgramacionTema1Ejercicio8/src/AreaRectangulo.java
@@ -0,0 +1,16 @@
+public class AreaRectangulo {
+    public static void main(String[] args) {
+
+        // Declaramos las variables
+        int base = 5;
+        int altura = 3;
+
+        // Calculamos el área del rectángulo
+        int area = base * altura;
+
+        // Mostramos los resultados
+        System.out.println("Base del rectángulo: " + base);
+        System.out.println("Altura del rectángulo: " + altura);
+        System.out.println("El área del rectángulo es: " + area);
+    }
+}
