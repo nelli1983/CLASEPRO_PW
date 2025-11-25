c59e693c395e5fb696d7cd19aaf7b5dc7b205820
nelli1983 <nelli.khachaturova@gmail.com>
2025-11-25 04:44:59 +0100

    ProgramacionTema1Ejercicio7

---
+++ ProgramacionTema1Ejercicio7/src/InformacionPersonal.java
@@ -0,0 +1,13 @@
+public class InformacionPersonal {
+    public static void main (String [] args) {
+        //Declaramos las variables
+        String nombre = "Elena";
+        String apellido = "García";
+        int edad = 24;
+        String ciudad = "Barcelona";
+
+        //Mostramos el mensaje de presentación
+        System.out.println("¡Hola! Me llamo"+nombre+" "+apellido+".");
+        System.out.println("Tengo "+edad+" años y vivo en "+ciudad+".");
+    }
+}
